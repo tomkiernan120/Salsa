9d46350ec3f01c7e55da0da8cc5092178644d4ad
Tom Kiernan <tkiernan120@gmail.com>
2018-09-24 16:08:54 +0100

    Initial commit

---
+++ src/Salsa.php
@@ -0,0 +1,168 @@
+<?php
+namespace Salsa;
+
+class Salsa
+{
+
+	private $routes = array();
+
+	private $namedRoutes = array();
+
+	private $basePath = '';
+
+	private $matchTypes = array();
+
+	public function __construct( $routes = array(), $basePath = '', $matchTypes = array() )
+	{
+		$this->addRoutes( $routes );
+		$this->setBasePath( $basePath );
+		$this->addMatchTypes( $matchTypes );
+	}
+
+	public function addRoutes( $routes )
+	{
+		if( !is_array( $routes ) ){
+			throw new \Excpetion( 'Routes shoule be an array' );
+		}
+		else {
+			foreach( $routes as $route ){
+				call_user_func_array(array( $this, 'map' ), $route );
+			}
+		}
+	}
+
+	public function map( $method, $route, $target, $name = null )
+	{
+		$this->routes[] = array( $method, $route, $target, $name );
+
+		if( $name ){
+			if( isset( $this->namedRoutes[$name] ) ){
+				throw new \Excpetion( "Can not redeclare route {$name}" );
+			}
+			else {
+				$this->namedRoute[$route] = $route;
+			}
+		}
+		return;
+	}
+
+	public function match( $requestUrl = null, $requestMethod = null )
+	{
+		$params = array();
+		$match = false;
+
+		if( $requestUrl === null ){
+			$requestUrl = isset( $_SERVER["REQUEST_URI"] ) ? $_SERVER["REQUEST_URI"] : "/";
+		}
+
+		// remove baseURL
+		$requestUrl = substr( $requestUrl, strlen( $this->basePath ) );
+
+		// remove
+		if( ( $strpos = strpos( $requestUrl, "?" )) !== false ){
+			$requestUrl = substr( $requestUrl, 0, $strpos );
+		}
+
+
+		if( $requestmode === null ){
+			$requestmode = isset( $_SERVER["REQUEST_METHOD"] ) ? $_SERVER["REQUEST_METHOD"] : "GET";
+		}
+
+		foreach( $this->routes as $handler ){
+			list( $methods, $route, $target, $name ) = $handler;
+
+			$method_match = ( stripos( $methods, $requestMethod ) !== false );
+
+			// Method did not match continue to next route
+
+			if( !$method_match ){
+				continue;
+			}
+
+			if( $route === "*" ){
+				// wildcard matches all
+				$match = true;
+			} elseif ( ( $position = strpos( $route, '[' ) ) === false ) {
+				// No params in url, do string comparison
+				$match = strcmp($requestUrl, $route) === 0;
+			} else {
+				// comapre request routewith route 
+				if( strncmp( $requestUrl, $route, $position ) !== 0 ){
+					continue;
+				}
+
+				$regex = $this->compiteRoute( $route );
+
+				$match = preg_replace( $regex, $requestUrl, $params ) === 1;
+			}
+
+			if( $match ){
+
+				if( $params ){
+					foreach( $params as $key => $value ){
+						if( is_numeric( $key ) ){
+							unset( $params[$key] );
+						}
+					}
+				}
+
+				return array(
+					'target' => $target,
+					'params' => $params,
+					'name' => $name
+				);
+
+			}
+
+		}
+
+		return false;
+	}
+
+	public function setBasePath( $basePath )
+	{
+		$this->basePath = $basePath;
+	}
+	
+	public function addMatchTypes($matchTypes)
+	{
+		$this->matchTypes = array_merge($this->matchTypes, $matchTypes);
+	}
+
+	public function compileRoute( $route )
+	{
+		if( preg_match_all( '`(/|\.|)\[([^:\]*+)(?::([^:\]]*+))?\](\?|)`', $route, $matches, PREG_SET_ORDER ) ){
+
+
+			$matchTypes = $this->matchTypes;
+
+			foreach( $matches as $match ){
+				list( $block, $pre, $type, $param, $optional ) = $match;
+
+				if( isset( $matchTypes[$type] ) ){
+					$type = $matchTypes[$type];
+				} 
+				if( $pre === "." ){
+					$pre = "\.";
+				}
+
+				$options = $optional !== '' ? '?' : null;
+
+				$pattern = '(?:' 
+					. ( $pre !== '' ? $pre : null )
+					. '('
+					. (  $param !== '' ? "?p<{$param}>" : null )
+					. $type 
+					. ')'
+					. $optional
+					. ')'
+					. $optional;
+				$route = str_replace( $block, $patther, $route );
+			}
+
+		}
+
+		return "`^$route$`u";
+	}
+
+}
