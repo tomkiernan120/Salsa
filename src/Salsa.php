9de439030113d8dd60a22dc7448d22a399fa89a9
Tom Kiernan <tkiernan120@gmail.com>
2018-10-02 11:42:16 +0100

    WIP

--- src/Salsa.php
+++ src/Salsa.php
@@ -48,11 +48,13 @@ class Salsa
 	public function setMethod( string $method )
 	{
 		$this->method = $method;
+    return $this;
 	}
 
 	public function setBaseRoute( string $base = "" )
 	{
 		$this->baseRoute = strtolower($base);
+    return $this;
 	}
 
 	public function getBaseRoute()
@@ -78,7 +80,6 @@ class Salsa
 	{
 		if( $handler ){
 				$this->handler = is_array($handler) ? $handler[0] : $handler;	
-				var_dump( $this->handler );
 		}
 	}
 
@@ -87,19 +88,31 @@ class Salsa
 		return $this->handler;
 	}
 
+  public function setParams( $params = array() )
+  {
+    $this->params = $params;
+    return $this;
+  }
+
+  public function getParams()
+  {
+    return $this->params;
+  }
+
 	public function addRoute( $route, $params, $method = "GET|POST|PUT|DELETE" )
 	{
 		$this->routes[strtolower($route)][$method] = $params;
+    return $this;
 	}
 
 	public function run()
 	{
+    // TODO: tidy up routing big style!!
+    // FIX: really need to stop supressing warnings
 		$route = $this->setCurrentRoute();
 		$methods = explode( "|", @array_keys( @$this->routes[$route] )[0]);
 		
-		var_dump( array_values( $this->routes[$route] )  );
-
-		$this->setHandler( array_values($this->routes[$route]) );
+		$this->setHandler( @array_values($this->routes[$route]) );
 
 		if( in_array( $_SERVER["REQUEST_METHOD"], $methods ) ){
 			$this->setmethod( $_SERVER["REQUEST_METHOD"] );
@@ -120,15 +133,79 @@ class Salsa
 		exit;
 	}
 
+  public function getRegex( $pattern )
+  {
+    if( preg_match( '/[^-:\/_{}()a-zA-Z\d]/', $pattern ) ){
+      return false; // invalid
+    }
+
+    // turn "(/)" int "/?"
+    $pattern = preg_replace( '#\(/\)#', '/?', $pattern );
+
+    $allowedCharacters = '[a-zA-Z0-9\_\-]+';
+    $pattern = preg_replace(
+      '/:( '.$allowedCharacters.' )/', // replace ":param"
+      '(?<$1>'. $allowedCharacters .')', // with "(?<param>[a-zA-Z0-9\_\-]+)"
+      $pattern
+    );
+
+    // capture group for {param}
+    $pattern = preg_replace(
+      '/{('.$allowedCharacters.')}/',
+      '(?<$1>'.$allowedCharacters.')'
+    );
+
+
+    $patternAsRegex = "@^" . $pattern . "$@D";
+
+    return $patternAsRegex;
+  }
+
+  public function parseRegex()
+  {
+    $url = $this->getCurrentRoute();
+
+    $patterAsRegex = $this->getRegex( $url );
+
+    if( $ok = !!$patterAsRegex ){
+
+      // if( $ok = preg_match( $patternAsRegex, $ ) ){
+
+      // } 
+      // needs to check current route agaisnt match
+
+    }
+
+  }
+
 	public function process()
 	{
-		if( isset( $this->handler ) ){
-			$type = gettype($this->handler);
-			var_dump( $type );
-			if( is_object( $type ) && is_callable( $type ) ){
-				call_user_func_array( $type, @$params = array() );
-			}
-		}
-	}
+    if( !isset( $this->handler ) ){
+      return false;
+    }
 
+
+		$type = gettype($this->handler);
+
+    $this->setParams();
+
+		if( is_object( $this->handler ) && is_callable( $this->handler ) ){
+			$return = call_user_func_array( $this->handler, $this->getParams() );
+      if( is_string( $return ) ){
+        echo $return;
+      }
+    }
+    else if( is_array( $this->handler ) && !empty( $this->handler ) ){
+      if( isset( $this->handler["controller"] ) && class_exists( $this->handler["controller"] ) ){
+        $controller = new $this->handler["controller"];
+        if( method_exists( $controller , $this->handler["method"] ) ){
+          $return = $controller->{$this->handler["method"]}( @$this->handler["passin"] );
+          if( is_string( $return ) ){
+            echo $return;
+          } 
+        }
+      }
+    }
+	}
+  
 }
